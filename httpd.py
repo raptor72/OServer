96b383ec41463f20b1abce529bef50753b3d9126
raptor72 <mctaras51@yandex.ru>
2019-12-22 22:44:55 +0400

    Add test html file with space

--- httpd.py
+++ httpd.py
@@ -86,8 +86,8 @@ def generate_response(request):
 def run(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#    server_socket.bind(('127.0.0.1', port))
-    server_socket.bind(('172.17.0.2', port))
+    server_socket.bind(('127.0.0.1', port))
+#    server_socket.bind(('172.17.0.2', port))
     server_socket.listen()
 
     while True:
